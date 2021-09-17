9af1992090e38111b52b03cd70e563d4ea597e38
Brianna Guarino <guarino.bb@gmail.com>
2021-09-17 17:14:49 -0500

    need help with looping

---
+++ myscript.js
@@ -0,0 +1,40 @@
+let humanWins = 0;
+let computerWins = 0;
+gameWinner = '';
+
+let playerSelection = prompt(`Enter 'rock', 'paper', or 'scissors'.`);
+
+function computerPlay() {
+  let choices = ['rock', 'paper', 'scissors']
+  return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function playRound(playerSelection, computerSelection) {
+
+  if (playerSelection === computerSelection) {
+    return `It's a tie! Score: ${humanWins}:${computerWins}`;
+  }
+  else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'rock')) {
+    humanWins++;
+    gameWinner = 'player';
+    return `Player wins! ${playerSelection} beats ${computerSelection}! Score: ${humanWins}:${computerWins}`;
+  }
+
+  else {
+    computerWins++;
+    gameWinner = 'computer';
+    return `You lose! ${computerSelection} beats ${playerSelection}. Score: ${humanWins}:${computerWins}`;
+  }
+}
+
+
+for (i = 0; i < 5; i++) {
+  let playerSelection = prompt(`Enter 'rock', 'paper', or 'scissors'.`);
+  let computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
+}
+
+
+
+
+
